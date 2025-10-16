6653a1ce8796eff9b1e9ca716d69bc31c1f82f50
mofopeadegoke <mofopeadegoke@gmail.com>
2025-10-16 15:53:35 +0300

    glow reduced a bit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ controls.dampingFactor = 0.03;
 const renderScene = new RenderPass(scene, camera);
 const bloomPass = new UnrealBloomPass(new THREE.Vector2(w, h), 1.5, 0.4, 100);
 bloomPass.threshold = 0.002;
-bloomPass.strength = 2;
+bloomPass.strength = 1;
 bloomPass.radius = 0;
 const composer = new EffectComposer(renderer);
 composer.addPass(renderScene);
